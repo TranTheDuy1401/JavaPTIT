cf43bc1673573d224bb742e29022593a713c7494
Trần Thế Duy <Ttheduy1401@gmail.com>
2025-09-03 13:40:48 +0700

    Update J07009_LOPINTSET-2.java

--- J07009_LOPINTSET-2.java
+++ J07009_LOPINTSET-2.java
@@ -58,18 +58,6 @@ public class J07009 {
         IntSet s3 = s1.intersection(s2);
         System.out.println(s3);
     }
-    public static void main3412886(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File("DATA.in"));
-        int n = sc.nextInt(), m = sc.nextInt(), a[] = new int[n], b[] = new int[m];
-        for (int i = 0; i < n; i++)
-            a[i] = sc.nextInt();
-        for (int i = 0; i < m; i++)
-            b[i] = sc.nextInt();
-        IntSet s1 = new IntSet(a);
-        IntSet s2 = new IntSet(b);
-        IntSet s3 = s1.intersection(s2);
-        System.out.println(s3);
-    }
 
 }
 
